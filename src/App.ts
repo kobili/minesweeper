b8ae8476afb250e906618b8c4263b7beb0e7b3a6
Kobe Li <likobe23@gmail.com>
2021-03-17 15:54:08 -0700

    added mine generation

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,5 @@
 import Minefield from "./Minefield"
 
-let mineField = new Minefield(3, 3, 3);
+let mineField = new Minefield(9, 9, 10);
 
 mineField.test();
--- src/Minefield.ts
+++ src/Minefield.ts
@@ -2,7 +2,7 @@ class Minefield {
     height: number;
     width: number;
     numMines: number;
-    field: Array< Array<String> > = [];
+    field: Array< Array<String> >;
 
     /**
      * A minefield is a grid of squares
@@ -14,32 +14,54 @@ class Minefield {
         this.height = height;
         this.width = width;
         this.numMines = numMines;
+
+        this.field = new Array< Array<String> >(height);
+        for (let i = 0; i < height; i++) {
+            this.field[i] = new Array<String>(width);
+        }
     }
 
     // Creates the MineField
     generateField() {
         this.intializeField();
+        this.initializeMines();
     }
 
     // Fills the field with null values in the shape (height, width)
     intializeField() {
-        let gridRow: Array<String> = [];
+
         for (let i = 0; i < this.height; i++) {
-            gridRow[i] = "_";
-        }
-        for (let i = 0; i < this.height; i++) {
-            this.field[i] = gridRow;
+            for (let j = 0; j < this.width; j++) {
+                this.field[i][j] = "_";
+            }
         }
     }
 
-    // Populates the field with mines randomly scattered around
-    intializeMines() {
+    // Populates the field with numMines mines randomly scattered around
+    initializeMines() {
         let m: number = this.numMines;
 
         while (m != 0) {
+            let x: number = this.randomIntegerBetween(0, this.width);
+            let y: number = this.randomIntegerBetween(0, this.height);
 
+            if (this.field[x][y] === "*") {
+                // this grid space is already occupied by a mine
+                continue;
+            }
+
+            this.field[x][y] = "*";
             m--;
         }
+    }
+
+    /**
+     * @param min : the random integer is greater than or equal to min
+     * @param max : the random integer is smaller than max
+     * @returns : random integer in the range [min, max)
+     */
+    randomIntegerBetween(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min) + min);
     }
 
     test() {
