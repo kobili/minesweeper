0f8d7a2dea24c8cb9331c767e6ceb803dd066aaa
Kobe Li <likobe23@gmail.com>
2021-03-17 18:39:03 -0700

    added flagging to Square.tsx

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -9,20 +9,34 @@ let Square = (props: any) => {
     // STATE: whether or not this square's label is hidden
     let [isHidden, setIsHidden] = useState(false);
 
+    // STATE:: whether or not this square has been flagged by the user
+    let [isFlagged, setIsFlagged] = useState(false);
+
     // when the user clicks on the square, the label is revealed
-    let revealLabel = () => {
-        setIsHidden(true);
+    let revealLabel = (e: any) => {
+        if (!isFlagged) {
+            setIsHidden(true);
+        }
+    }
+
+    // place a flag on the square when the user right clicks on it
+    // if already flagged, then remove the flag on right click
+    let flagSquare = (e: any) => {
+        e.preventDefault();
+        setIsFlagged(!isFlagged);
     }
 
     let label: String;
     if (isHidden) {
         label = props.val;
-    } else {
+    } else if (isFlagged) {
+        label = "🇨🇦";
+    }else {
         label = "";
     }
 
     return (
-        <button className="square" onClick={revealLabel}>{label}</button>
+        <button className="square" onClick={revealLabel} onContextMenu={flagSquare}>{label}</button>
     )
 }
 
